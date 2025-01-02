92a9b7d8770109a5bc83bf33dfdbd85b1555eece
Aditya Kr <148944465+4mskumar@users.noreply.github.com>
2025-01-02 21:22:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FollowCursor from "./Components/FollowCursor";
 import Hero from "./Components/Hero";
-import NavBar from "./Components/NavBar";
+import Navbar from "./Components/NavBar";
 import Testimonial from './Components/Testimonial';
 import Our_feat from './Components/Our_feat';
 import History from './Components/History';
@@ -14,7 +14,7 @@ const App = () => {
       <FollowCursor />
 
       {/* Navbar */}
-      <NavBar />
+      <Navbar />
 
       {/* Hero section */}
       <Hero />
